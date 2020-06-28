da6474acc80d1e5b05d9985d18cfe63c7b28b0f2
why39 <whyiot39@163.com>
2020-06-28 20:06:48 +0800

    整合案卡项世系可视化和导入日志生成世系功能

--- src/main/java/com/hxy/modules/activiti/controller/LogImporter.java
+++ src/main/java/com/hxy/modules/activiti/controller/LogImporter.java
@@ -1,4 +1,38 @@
 package com.hxy.modules.activiti.controller;
 
+import com.hxy.modules.common.utils.Result;
+import com.hxy.provenance.log.caseLogBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedCaseInsensitiveMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
 public class LogImporter {
+    /**
+     *  导入日志
+     */
+    @RequestMapping(value="/logImport", method = RequestMethod.POST)
+    @ResponseBody
+    public Result logImport(javax.servlet.http.HttpServletRequest request, Model model){
+        Result result = null;
+        try{
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            Map<String, Object> params = new LinkedCaseInsensitiveMap<>();
+            for (String key : parameterMap.keySet()) {
+                params.put(key, parameterMap.get(key)[0]);
+            }
+            String caseId = (String)params.get("caseId");
+            caseLogBuilder.caseLogBuild(caseId);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            result = Result.error("导入日志失败");
+        }
+        return result;
+    }
 }
