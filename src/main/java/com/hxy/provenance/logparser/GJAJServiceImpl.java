cf49d3410f6bde4fd4081003afcc1076587be91f
wangxianpeng <wisimer@163.com>
2020-08-15 13:26:29 +0800

    neod.html&先添加环节再添加案卡项信息

--- src/main/java/com/hxy/provenance/logparser/GJAJServiceImpl.java
+++ src/main/java/com/hxy/provenance/logparser/GJAJServiceImpl.java
@@ -96,72 +96,85 @@ public class GJAJServiceImpl implements GJAJService {
         //2. 从RZ_YX_RZ中查询该BMSAH所有的日志
         List<GJRZEntity> rzlist = logDao.queryList(BMSAH); //按时间升序排列
         try {
-            for (GJRZEntity rz : rzlist) {
-                String RZMS = rz.getRZMS();
-                if (RZMS != null && RZMS.length() > 0) {
-                    if (!rz.getEJFL().isEmpty() && rz.getEJFL().startsWith(GJRZEntity.LC_PREFIX)) {
+            //2.1 先添加流程信息：
+            for (GJRZEntity lcrz : rzlist) {
+                String LCRZMS = lcrz.getRZMS();
+                if (LCRZMS != null && LCRZMS.length() > 0) {
+                    if (!lcrz.getEJFL().isEmpty() && lcrz.getEJFL().startsWith(GJRZEntity.LC_PREFIX)) {
                         //流程环节节点
                         Map<String, Object> map = new HashMap<>();
                         map.put(NeoConstants.KEY_LAST_NODE_ID, caseNodeId);
-                        map.put("name", RZMS);
-                        map.put("CZRM", rz.getCZRM());
-                        map.put("操作人", RZMS);
-                        map.put("RZID", rz.getID());
+                        map.put("name", LCRZMS);
+                        map.put("CZRM", lcrz.getCZRM());
+                        map.put("操作人", LCRZMS);
+                        map.put("RZID", lcrz.getID());
                         GJNeo4jUtil.addTaskNode(BMSAH, "Task", "next", false, map);
 
-                    } else if (RZMS.startsWith("[") || RZMS.startsWith("{")) {
-
-                        //解析json
-
-                        //正则匹配键值对，字符型的值
-                        Pattern r1 = Pattern.compile("\"(\\w+)\":\"{1}(.*?)\"{1}");
-                        Matcher m1 = r1.matcher(RZMS);
-                        while (m1.find()) {
-                            String key = m1.group(1);
-                            String value = m1.group(2);
-                            System.out.println("xp>>>>>>>>>Found key: " + m1.group(1));
-                            System.out.println("xp>>>>>>>>>Found value: " + m1.group(2));
-                            Map<String, Object> par = new HashMap<>();
-                            par.put(key, value);
-                            par.put("CaseNodeId", caseNodeId);
-                            par.put("name", key);
-                            par.put("操作人", rz.getCZRM());
-                            par.put("RZID", rz.getID());
-                            GJNeo4jUtil.addPropertyNode(BMSAH, key, "next", false, par);
-                        }
-
-                        //正则匹配键值对，整型的值
-                        Pattern r2 = Pattern.compile("\"(\\w+)\":(\\w+)");
-                        Matcher m2 = r2.matcher(RZMS);
-                        while (m2.find()) {
-                            String key = m2.group(1);
-                            String value = m2.group(2);
-                            System.out.println("xp>>>>>>>>>Found key: " + m2.group(1));
-                            System.out.println("xp>>>>>>>>>Found value: " + m2.group(2));
-                            Map<String, Object> par = new HashMap<>();
-                            par.put(NeoConstants.KEY_LAST_NODE_ID, caseNodeId);
-                            par.put(key, value);
-                            par.put("CaseNodeId", caseNodeId);
-                            par.put("name", key);
-                            par.put("操作人", rz.getCZRM());
-                            par.put("RZID", rz.getID());
-                            GJNeo4jUtil.addPropertyNode(BMSAH, key, "next", false, par);
-                        }
-
-
-                    } else {
-                        //不是json，直接创建节点
-                        Map<String, Object> map = new HashMap<>();
-                        map.put(NeoConstants.KEY_LAST_NODE_ID, caseNodeId);
-                        map.put("RZMS", RZMS);
-                        map.put("name", "ACTION");
-                        map.put("操作人", rz.getCZRM());
-                        map.put("RZID", rz.getID());
-                        GJNeo4jUtil.addActionNode(BMSAH, "ACTION", "next", false, map);
                     }
+
                 }
+            }
 
 
+            //2.2再添加其他日志信息：
+            for (GJRZEntity rz : rzlist) {
+                String RZMS = rz.getRZMS();
+                if (RZMS != null && RZMS.length() > 0) {
+                    if (rz.getEJFL().isEmpty() || !rz.getEJFL().startsWith(GJRZEntity.LC_PREFIX)) {
+                        if (RZMS.startsWith("[") || RZMS.startsWith("{")) {
+
+                            //解析json
+
+                            //正则匹配键值对，字符型的值
+                            Pattern r1 = Pattern.compile("\"(\\w+)\":\"{1}(.*?)\"{1}");
+                            Matcher m1 = r1.matcher(RZMS);
+                            while (m1.find()) {
+                                String key = m1.group(1);
+                                String value = m1.group(2);
+                                System.out.println("xp>>>>>>>>>Found key: " + m1.group(1));
+                                System.out.println("xp>>>>>>>>>Found value: " + m1.group(2));
+                                Map<String, Object> par = new HashMap<>();
+                                par.put(key, value);
+                                par.put("CaseNodeId", caseNodeId);
+                                par.put("name", key);
+                                par.put("操作人", rz.getCZRM());
+                                par.put("RZID", rz.getID());
+                                GJNeo4jUtil.addPropertyNode(BMSAH, key, "next", false, par);
+                            }
+
+                            //正则匹配键值对，整型的值
+                            Pattern r2 = Pattern.compile("\"(\\w+)\":(\\w+)");
+                            Matcher m2 = r2.matcher(RZMS);
+                            while (m2.find()) {
+                                String key = m2.group(1);
+                                String value = m2.group(2);
+                                System.out.println("xp>>>>>>>>>Found key: " + m2.group(1));
+                                System.out.println("xp>>>>>>>>>Found value: " + m2.group(2));
+                                Map<String, Object> par = new HashMap<>();
+                                par.put(NeoConstants.KEY_LAST_NODE_ID, caseNodeId);
+                                par.put(key, value);
+                                par.put("CaseNodeId", caseNodeId);
+                                par.put("name", key);
+                                par.put("操作人", rz.getCZRM());
+                                par.put("RZID", rz.getID());
+                                GJNeo4jUtil.addPropertyNode(BMSAH, key, "next", false, par);
+                            }
+
+
+                        } else {
+                            //不是json，直接创建节点
+                            Map<String, Object> map = new HashMap<>();
+                            map.put(NeoConstants.KEY_LAST_NODE_ID, caseNodeId);
+                            map.put("RZMS", RZMS);
+                            map.put("name", "ACTION");
+                            map.put("操作人", rz.getCZRM());
+                            map.put("RZID", rz.getID());
+                            GJNeo4jUtil.addActionNode(BMSAH, "ACTION", "next", false, map);
+                        }
+                    }
+
+
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
