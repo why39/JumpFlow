bbfc5e8095cf2b20e7025aa9ca5a8b9fe22e1af6
wangxianpeng <wisimer@163.com>
2021-05-10 16:54:21 +0800

    dq mysql table

--- src/main/java/com/hxy/dq/Suggestion.scala
+++ src/main/java/com/hxy/dq/Suggestion.scala
@@ -2,26 +2,45 @@ package com.hxy.dq
 
 import org.apache.spark.sql.SparkSession
 import java.io._
+import java.util.{Properties, UUID}
+
+import com.amazon.deequ.VerificationResult.checkResultsAsDataFrame
+import com.amazon.deequ.VerificationSuite
+import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
+import com.amazon.deequ.constraints.ConstraintStatus
 import com.amazon.deequ.suggestions.{ConstraintSuggestionRunner, Rules}
+
 object Suggestion {
-  val path = "/Users/mac/xp/projects/cscw/NewBpm/JumpFlow/"
+  val path = "/Users/mac/xp/JumpFlow/case/"
+
   def main(args: Array[String]) {
-    deal("sample_file1.csv")
+    deal(path+"sample_file" + UUID.randomUUID())
   }
 
-  def deal(outputname:String): Unit ={
+  def deal(outputname: String): Unit = {
     val spark = SparkSession.builder
       .appName("Deequ")
       .master("local[*]")
       .getOrCreate()
     import spark.implicits._
-    val dF = spark.read
-      .option("header", true)
-      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
-      .csv(path+"titanic.csv")
 
+    //    val dF = spark.read
+    //      .option("header", true)
+    //      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+    //      .csv(path+"titanic.csv")
+
+    val properties = new Properties()
+    properties.setProperty("user", "root")
+    properties.setProperty("password", "19921015")
+
+    val dF = spark.read
+      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+      .jdbc("jdbc:mysql://localhost:3306/hxyframe_activiti?allowMultiQueries=true&useUnicode=true&useSSL=false&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true", "properties_mapper", properties)
+      .select("en", "cn")
 
     dF.printSchema()
+
+    // val writer = new PrintWriter(new File("test.txt"))
 
     val suggestionResult = {
       ConstraintSuggestionRunner()
@@ -37,12 +56,42 @@ object Suggestion {
       }
     }.toSeq.toDS()
     suggestionDataFrame.show(truncate = false)
-    //    suggestionDataFrame.toJSON.show()
-    suggestionDataFrame.coalesce(1).write
+
+
+    val verificationResult = VerificationSuite()
+      .onData(dF)
+      .addCheck(
+        Check(CheckLevel.Error, "DQ verification")
+          .hasSize(_ > 0)
+          .isComplete("en")
+          .isUnique("en")
+          .isComplete("en")
+          .isNonNegative("en"))
+      .run()
+
+    if (verificationResult.status == CheckStatus.Success) {
+      println("The data passed the test, everything is fine!")
+    } else {
+      println("We found errors in the data:\n")
+
+      val resultsForAllConstraints = verificationResult.checkResults
+        .flatMap { case (_, checkResult) => checkResult.constraintResults }
+
+      resultsForAllConstraints
+        .filter {
+          _.status != ConstraintStatus.Success
+        }
+        .foreach { result => println(s"${result.constraint}: ${result.message.get}") }
+    }
+    val resultDataFrame = checkResultsAsDataFrame(spark, verificationResult)
+    resultDataFrame.coalesce(1).write.format("csv")
+      .option("header", "false")
+      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+      .mode("Append").save(outputname)
+    resultDataFrame.coalesce(1).write
       .option("header", "true")
       .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
-      .csv(path+"case/"+outputname)
-    //    suggestionDataFrame.write.csv("test.csv")
-    //    suggestionDataFrame.write.json("test.json")
+      .mode("Append").json(outputname)
+
   }
 }
