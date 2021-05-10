10e33341336a262e9f51450b3ddbb3dbc99d0b2c
wangxianpeng <wisimer@163.com>
2021-05-10 17:24:27 +0800

    dq mysql ak

--- src/main/java/com/hxy/dq/Suggestion.scala
+++ src/main/java/com/hxy/dq/Suggestion.scala
@@ -1,42 +1,39 @@
 package com.hxy.dq
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import java.io._
 import java.util.{Properties, UUID}
 
 import com.amazon.deequ.VerificationResult.checkResultsAsDataFrame
 import com.amazon.deequ.VerificationSuite
 import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
-import com.amazon.deequ.constraints.ConstraintStatus
+import com.amazon.deequ.constraints.{ConstrainableDataTypes, ConstraintStatus}
 import com.amazon.deequ.suggestions.{ConstraintSuggestionRunner, Rules}
 
 object Suggestion {
   val path = "/Users/mac/xp/JumpFlow/case/"
+  val spark = SparkSession.builder
+    .appName("Deequ")
+    .master("local[*]")
+    .getOrCreate()
+
+  import spark.implicits._
 
   def main(args: Array[String]) {
-    deal(path+"sample_file" + UUID.randomUUID())
+    dealAKX(path + "res_" + UUID.randomUUID())
   }
 
-  def deal(outputname: String): Unit = {
+  def deal(dF: DataFrame, check: Check, outputname: String): Unit = {
     val spark = SparkSession.builder
       .appName("Deequ")
       .master("local[*]")
       .getOrCreate()
     import spark.implicits._
 
-    //    val dF = spark.read
-    //      .option("header", true)
-    //      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
-    //      .csv(path+"titanic.csv")
-
     val properties = new Properties()
     properties.setProperty("user", "root")
     properties.setProperty("password", "19921015")
 
-    val dF = spark.read
-      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
-      .jdbc("jdbc:mysql://localhost:3306/hxyframe_activiti?allowMultiQueries=true&useUnicode=true&useSSL=false&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true", "properties_mapper", properties)
-      .select("en", "cn")
 
     dF.printSchema()
 
@@ -61,12 +58,9 @@ object Suggestion {
     val verificationResult = VerificationSuite()
       .onData(dF)
       .addCheck(
-        Check(CheckLevel.Error, "DQ verification")
-          .hasSize(_ > 0)
-          .isComplete("en")
-          .isUnique("en")
-          .isComplete("en")
-          .isNonNegative("en"))
+        check
+      )
+
       .run()
 
     if (verificationResult.status == CheckStatus.Success) {
@@ -92,6 +86,92 @@ object Suggestion {
       .option("header", "true")
       .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
       .mode("Append").json(outputname)
+  }
+
+  //嫌疑人表
+  def dealXYR(outputname: String): Unit = {
+
+
+    val properties = new Properties()
+    properties.setProperty("user", "root")
+    properties.setProperty("password", "19921015")
+
+    val dF = spark.read
+      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+      .jdbc("jdbc:mysql://localhost:3306/hxyframe_activiti?allowMultiQueries=true&useUnicode=true&useSSL=false&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true", "tb_dq_fzxyr", properties)
+      .select("id", "xyrxm", "xyrbh", "afsnl", "xb")
+
+    dF.printSchema()
+
+
+    var check = Check(CheckLevel.Error, "person verification")
+      .isComplete("id")
+      .isUnique("id")
+      .hasDataType("id", ConstrainableDataTypes.Integral)
+
+      .isComplete("xyrxm")
+      .hasDataType("xyrxm", ConstrainableDataTypes.String)
+
+      .isComplete("xyrbh")
+      .hasDataType("xyrbh", ConstrainableDataTypes.String)
+
+      .isComplete("afsnl")
+      .isNonNegative("afsnl")
+      .isLessThan("afsnl", "120")
+      .hasDataType("afsnl", ConstrainableDataTypes.Integral)
+
+      .isComplete("xb")
+      .isContainedIn("xb", Array("male", "female"))
+      .hasDataType("xb", ConstrainableDataTypes.String)
+
+    deal(dF, check, outputname)
+
+  }
+
+
+  //嫌疑人表
+  def dealAKX(outputname: String): Unit = {
+
+
+    val properties = new Properties()
+    properties.setProperty("user", "root")
+    properties.setProperty("password", "19921015")
+
+    val dF = spark.read
+      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
+      .jdbc("jdbc:mysql://localhost:3306/hxyframe_activiti?allowMultiQueries=true&useUnicode=true&useSSL=false&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true", "tb_dq_ak", properties)
+      .select("id", "AFD_MC", "AJCBDW_MC", "AJCBDW_BM", "AJLB_MC", "AJLB_BM", "AY_MC", "AY_DM")
+
+    dF.printSchema()
+
+
+    var check = Check(CheckLevel.Error, "case verification")
+      .isComplete("id")
+      .isUnique("id")
+      .hasDataType("id", ConstrainableDataTypes.Integral)
+
+      .isComplete("AFD_MC")
+      .hasDataType("AFD_MC", ConstrainableDataTypes.String)
+
+      .isComplete("AJCBDW_MC")
+      .hasDataType("AJCBDW_MC", ConstrainableDataTypes.String)
+
+      .isComplete("AJCBDW_BM")
+      .isNonNegative("AJCBDW_BM")
+      .hasDataType("AJCBDW_BM", ConstrainableDataTypes.Integral)
+
+      .isComplete("AJLB_MC")
+      .hasDataType("AJLB_MC", ConstrainableDataTypes.String)
+
+      .isComplete("AY_MC")
+      .hasDataType("AJLB_MC", ConstrainableDataTypes.String)
+
+      .isComplete("AY_DM")
+      .isNonNegative("AY_DM")
+      .hasDataType("AY_DM", ConstrainableDataTypes.Integral)
+
+
+    deal(dF, check, outputname)
 
   }
 }
